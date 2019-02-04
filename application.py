119d6f50ed50e133f3ce30d31ab06aa6baf9d1c6
doruboyina <manjunath.imagin@gmail.com>
2019-02-04 11:08:47 +0530

    updated code

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Dude It Worked</h1>
+    <h1>Application deployed from Upshot Tech</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
