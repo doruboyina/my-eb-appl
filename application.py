6b6f93597e571243c899094dc254d59367f0ae8a
doruboyina <manjunath.imagin@gmail.com>
2019-02-14 23:51:23 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from wsgiref.simple_server import make_server
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# Handler 
+# Handler adding new content
 LOG_FILE = '/opt/python/log/sample-app.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
